3308669b11ee49291ed976da8a01d8318dd758b1
feb19 <nt@feb19.jp>
2025-07-08 10:31:53 +0900

    predeploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+const repo = 'mui-multiselect-sample';          // リポジトリ名
+const isProd = process.env.NODE_ENV === 'production';
+
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',                     // 静的サイト書き出し
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  images: { unoptimized: true },        // <Image> を使うなら必須
+  trailingSlash: true
 };
 
 export default nextConfig;
